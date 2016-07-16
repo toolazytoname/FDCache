4b3bdf1cf80fabe1124e0aa82734959636a989bb
chaowei <geekw@foxmail.com>
2016-07-16 08:54:55 +0800

    add empty project and git ignore file

---
+++ Demo/FDCacheDemo/FDCacheDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FDCacheDemo
+//
+//  Created by weichao on 16/7/16.
+//  Copyright © 2016年 chaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/FDCacheDemo/FDCacheDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FDCacheDemo
+//
+//  Created by weichao on 16/7/16.
+//  Copyright © 2016年 chaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
