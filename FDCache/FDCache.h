2ae9f159e3bb6a8ba921a58d937c4721be66faff
weichao <shuitaiyang747@qq.com>
2016-10-01 09:50:37 +0800

    Update FDCache.h

--- FDCache/FDCache.h
+++ FDCache/FDCache.h
@@ -13,7 +13,7 @@ typedef void(^cacheIsExitCompletedBlock)(BOOL isExist);
 typedef void(^cacheNoParamsBlock)();
 
 /**
- A simle cache manager class:reference to the SDImageCache in SDWebImage.
+ A simle cache manager class reference to the SDImageCache in SDWebImage.
  Basing on the file system, my class reads and writes into cache data through the key corresponding to the file.The problems lay in inconvenient extension, lacking of metadata, unavailiableness of the elimination algorithm, slowness to statistics.
  */
 @interface FDCache : NSObject
